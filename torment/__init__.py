17586b32a78c7835a4ca67d78d3ee63dac2621b5
Shawn Warren <shawn.warren@rackspace.com>
2016-01-14 15:20:55 -0600

    add nullhandler to logging config

--- torment/__init__.py
+++ torment/__init__.py
@@ -11,3 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.propogate = False
+LOGGER.addHandler(logging.NullHandler())
